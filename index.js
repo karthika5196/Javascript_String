3c1a7e21becd7c06aa2e3e30c79b35e521ec5eea
karthika5196 <73806407+karthika5196@users.noreply.github.com>
2023-05-17 16:47:15 +0800

    1 file modified

--- index.js
+++ index.js
@@ -4,131 +4,52 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
-//Sol 1:check whether an `input` is a string or not
-function is_String(str) {
-  let strType = typeof str;
-  console.log(strType);
-  if (strType == 'string') {
-    //Object.prototype.toString.call(input) === '[object String]'
-    return true;
-  } else {
-    return false;
+
+//Sol 1: find the sign of the product of three numbers.
+let num = 3 * 4 * 8;
+let sign = Math.sign(num) == -1 ? 'the sign is -' : 'the sign is +';
+console.log(sign);
+
+//Sol 2: sort three numbers
+let numbers = [3, -7, 100];
+console.log(
+  numbers.sort(function (num1, num2) {
+    return num1 - num2;
+  })
+);
+
+//Sol 3: loop that iterates from 0 to 15. For each iteration, it checks if the current number is odd or even, and displays a message on the screen
+function checkOddEven(n) {
+  let text = '';
+  for (let i = 0; i <= n; i++) {
+    if (i === 0 || i % 2 === 0) {
+      text += i + ' ' + 'even\n';
+    } else {
+      text += i + ' ' + 'odd\n ';
+    }
   }
-}
-console.log(is_String('Karthika'));
-
-//Sol 2:Check whether a string is blank or not
-function is_Blank(str) {
-  let strLength = str.length;
-  let val = strLength === 0 ? true : false;
-  return val;
-}
-console.log(is_Blank(''));
-
-//Sol 3: split a string and convert it into an array of words
-function string_to_Array(str) {
-  let split = str.trim().split(' ');
-  return split;
-}
-console.log(string_to_Array('Robin Fashion'));
-
-//Sol 4: extract a specified number of characters from a string
-function extractString(str, num) {
-  console.log(str.slice(0, num));
-}
-extractString('Robinhood', 5);
-
-//Sol 5: convert a string into abbreviated form
-function abbrevated_string(str) {
-  let split = str.trim().split(' ');
-  let abbrStr = split[0] + ' ' + split[1].charAt(0);
-  return abbrStr;
-}
-console.log(abbrevated_string('Rakesh Sharma'));
-
-//sol 6: parameterize a string
-function string_parameterize(str) {
-  let strParam = str.trim().toLowerCase().split(' ').join('-');
-  return strParam;
-}
-console.log(string_parameterize('Robin Singh from USA'));
-
-//sol 7: capitalize the first letter of a string
-function capitalize(str) {
-  /*   let split = str.trim().split('');
-  let cap = split[0].toUpperCase() + split.slice(1, split.length).join('');
-  return cap; */
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capitalize('robin mastro'));
-
-//Sol 8: capitalize_Words
-function capitalize_Words(str) {
-  let text = str.replace(/\w\S*/g, function (txt) {
-    console.log(txt);
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  });
   return text;
 }
-console.log(capitalize_Words('js string exercises'));
+console.log(checkOddEven(10));
 
-//Sol 9: converts upper case letters to lower case, and lower case letters to upper case
-
-function swapCase(str) {
-  return str.replace(/([a-z]+)|([A-Z]+)/g, function (match, chr) {
-    //second parameter is first matching regex group.
-    console.log(match, chr);
-    return chr ? match.toUpperCase() : match.toLowerCase();
-  });
-}
-console.log(swapCase('AaBb'));
-
-//Sol 10: string into camel case
-function camelCase(str) {
-  console.log(
-    str.replace(/\W+(.)/g, function (match, chr) {
-      console.log(match, chr);
-      return chr.toUpperCase();
-    })
-  );
-}
-camelCase('Javascript exercise');
-
-//Sol 11: uncommelize a string
-function unCommalize(str, separator) {
-  if (typeof separator == 'undefined') {
-    separator = ' ';
+//Sol 4:  iterates integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz". For numbers multiples of both three and five print "FizzBuzz".
+function fizzBuzz(n) {
+  let str = '';
+  for (let i = 1; i <= n; i++) {
+    str +=
+      i % 3 == 0 && i % 5 == 0
+        ? ' FizzBuzz\n'
+        : i % 3 == 0
+        ? ' Fizz\n'
+        : i % 5 == 0
+        ? ' Buzz\n'
+        : i + '\n';
   }
-  var str = str.replace(/[A-Z]/g, function (letter) {
-    console.log(letter);
-    return separator + letter.toLowerCase();
-  });
-  console.log(str);
-  return str.replace('/^' + separator + '/', '');
+  return str;
 }
-console.log(unCommalize('helloWorld', '_'));
+console.log(fizzBuzz(18));
 
-//Sol 12: concatenate a given string n times
-function repeat(str, n) {
-  if (n == undefined) {
-    n = 1;
-  }
-  return str.repeat(n);
-}
-console.log(repeat('hai', 4));
+//Sol 5: "Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers, while those that do not end in 1 are unhappy numbers (or sad numbers)". Write a JavaScript program to find and print the first 5 happy numbers.
 
-//Sol 13:  insert a string within a string at a particular position (default is 1)
-
-function insert(str, ins_str, position) {
-  if (position == undefined) {
-    position = 0;
-  }
-  if (ins_str == undefined) {
-    ins_str = ' ';
-  }
-  return str.slice(0, position) + ins_str + str.slice(position);
-}
-console.log(insert('we are doing some exercise ', 'javascript '));
-
-
-
+function isHappyNumber(num) {}
+console.log(isHappyNumber(49));
